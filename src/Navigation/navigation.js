17d162dccce3990f54a3b044b651f417dc5b0c0b
Roshan Khadka <roshan.khadka.u@gmail.com>
2022-06-17 20:20:10 +1000

    router changes

--- src/Navigation/navigation.js
+++ src/Navigation/navigation.js
@@ -1,7 +1,7 @@
 // http://www.emilyridge.ie/
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from "../Components/home";
 import { Portfolio } from "../Components/portfolio";
@@ -11,15 +11,15 @@ import projects from '../Api/projects-list'
 
 export function Navigation(){
     return(
-        <Router>
+        <>
             <Navbar collapseOnSelect expand="lg" bg="primary" variant="light" >
                 <Container>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto" style={{fontSize:"20px"}}>
-                  <Nav.Link href="/" style={{color:"white"}}>Home</Nav.Link>
-                  <Nav.Link href="/portfolio" style={{color:"white"}}>Portfolio</Nav.Link>
-                  <Nav.Link href="#contact-links" style={{color:"white"}}>Contact</Nav.Link>
+                  <Nav.Link href="#/" style={{color:"white"}}>Home</Nav.Link>
+                  <Nav.Link href="#/portfolio" style={{color:"white"}}>Portfolio</Nav.Link>
+                  <Nav.Link href="#/#contact-links" style={{color:"white"}}>Contact</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
                 </Container>
@@ -27,7 +27,7 @@ export function Navigation(){
 
              <Routes>
                 <Route  path="/" element={<Home />}>Home</Route>
-                <Route  path="portfolio" element={<Portfolio/>}></Route>
+                <Route  path="/portfolio" element={<Portfolio/>}></Route>
                 {startupprojects.map((project)=>{
                     return <Route  path={project.title} element={<ProjectDescription prop={project} />}></Route>                
                 })}
@@ -37,6 +37,6 @@ export function Navigation(){
                 {/* <Route exact path="/contact">Contact</Route> */}
 
              </Routes>
-         </Router>
+        </>
     )
 };
