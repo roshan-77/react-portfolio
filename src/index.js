f1544b93cd2e36c19ccefff117e73e87fe09cba9
Roshan Khadka <roshan.khadka.u@gmail.com>
2022-06-17 20:47:52 +1000

    router changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <Router>
     <React.StrictMode>
     <App />
     </React.StrictMode>
-  </HashRouter>
+  </Router>
   
 );
 
